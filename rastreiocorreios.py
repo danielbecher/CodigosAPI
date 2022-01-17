5a78ddb19de7bab039b180d653bd32634a987ed2
Daniel Becher <daniel@becher.com.br>
2022-01-16 22:27:47 -0300

    Comentários feitos no script

--- rastreiocorreios.py
+++ rastreiocorreios.py
@@ -1,30 +1,25 @@
-#ConsultaCEP
+#RASTREIO Correios 1.1
 
 #importa as bibliotecas
 import requests
 
-# cabeçalho decorado
+#cabeçalho decorado
 print('###'*25)
 print('                 RASTREIO DE ENCOMENDAS 1.1')
 print('###'*25)
 
-#pega o cep
+#Pega com o usuário o código da encomenda
 codigo_rastreio = input('Digite o código de rastreio para a consulta: ')
 
 #faz a conexão com a API da ViaCep
 request = requests.get('https://proxyapp.correios.com.br/v1/sro-rastro/{}'.format(codigo_rastreio))
 resp_consulta = (request.json())
-#print(resp_consulta)
-
-#Verifica se o CEP existe e retorna seus dados.
-continua = 's'
-#while continua == 'S':
-#if 'sro-020' in resp_consulta['objetos'][0]['mensagem'][0]:
-    #print(dadosendereco)
 
 listaeventos = []
 listaeventos2 = []
 i = 0
+#Se o código de rastreio for válido, vai fazer as devidas extrações com o JSON retornado dos Correios
+#e vai retornar a resposta.
 try:
     while i < len(resp_consulta['objetos'][0]['eventos'][0]):
         listaeventos.append(resp_consulta['objetos'][0]['eventos'][i])
@@ -44,6 +39,8 @@ try:
         print('|| Situação: [{}] - {}'.format(listaeventos2[x]['codigo'],listaeventos2[x]['descricao']))
         print('=-=' * 25)
         x += 1
+
+#Se der erro e o código não estiver obsoleto, vai informar que o código ainda não foi postado ou é inválido.
 except:
     print('Objeto não localizado ou código inválido. Verifique o código e se ele foi postado e tente novamente!')
 
